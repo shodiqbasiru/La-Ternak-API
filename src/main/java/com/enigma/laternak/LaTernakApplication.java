661165d11d02919b9f4784231f28d8e54b5915e5
shodiqbasiru <shodiqbasiru@gmail.com>
2024-05-03 22:20:11 +0700

    [IMPROVE] Change name package

--- src/main/java/com/enigma/laternak/LaTernakApplication.java
+++ src/main/java/com/enigma/laternak/LaTernakApplication.java
@@ -1,4 +1,4 @@
-package com.rnigma.laternak;
+package com.enigma.laternak;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
