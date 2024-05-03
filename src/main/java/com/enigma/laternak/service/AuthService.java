96e515553d90a1f5b0c441f2c8f00f3dd24acd39
shodiqbasiru <shodiqbasiru@gmail.com>
2024-05-03 22:30:03 +0700

    [FEATURE] add feat register customer

--- src/main/java/com/enigma/laternak/service/AuthService.java
+++ src/main/java/com/enigma/laternak/service/AuthService.java
@@ -1,4 +1,8 @@
 package com.enigma.laternak.service;
 
+import com.enigma.laternak.dto.request.AuthRequest;
+import com.enigma.laternak.dto.response.RegisterResponse;
+
 public interface AuthService {
+    RegisterResponse register(AuthRequest request);
 }
