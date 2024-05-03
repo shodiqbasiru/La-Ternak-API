1f2233701471a2692e7e13514da101c9b65c40d9
shodiqbasiru <shodiqbasiru@gmail.com>
2024-05-03 17:49:14 +0700

    Initial commit

---
+++ src/main/java/com/rnigma/laternak/LaTernakApplication.java
@@ -0,0 +1,13 @@
+package com.rnigma.laternak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaTernakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LaTernakApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rnigma/laternak/LaTernakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rnigma.laternak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaTernakApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
