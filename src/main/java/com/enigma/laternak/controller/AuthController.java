71cd4223e2cdc5c852f4cb6c527569642ccc7e30
shodiqbasiru <shodiqbasiru@gmail.com>
2024-05-03 22:28:34 +0700

    [FEATURE] add feat register customer

--- src/main/java/com/enigma/laternak/controller/AuthController.java
+++ src/main/java/com/enigma/laternak/controller/AuthController.java
@@ -1,4 +1,38 @@
 package com.enigma.laternak.controller;
 
+import com.enigma.laternak.constant.ApiRoute;
+import com.enigma.laternak.dto.request.AuthRequest;
+import com.enigma.laternak.dto.response.CommonResponse;
+import com.enigma.laternak.dto.response.RegisterResponse;
+import com.enigma.laternak.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(ApiRoute.AUTH_API)
 public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping(
+            path = "/register",
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<CommonResponse<?>> register(@RequestBody AuthRequest request){
+        RegisterResponse register = authService.register(request);
+        CommonResponse<RegisterResponse> response = CommonResponse.<RegisterResponse>builder()
+                .statusCode(HttpStatus.CREATED.value())
+                .message("Created a new account successfully")
+                .data(register)
+                .build();
+        return new ResponseEntity<>(response,HttpStatus.CREATED);
+    }
+
 }
