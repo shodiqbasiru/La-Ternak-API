feaec07f88cccd74d70f602dfe0bb336ec3abae9
shodiqbasiru <shodiqbasiru@gmail.com>
2024-05-05 23:29:17 +0700

    [IMPROVE] delete unnecessary import

--- src/main/java/com/enigma/laternak/dto/request/SearchProductRequest.java
+++ src/main/java/com/enigma/laternak/dto/request/SearchProductRequest.java
@@ -1,8 +1,5 @@
 package com.enigma.laternak.dto.request;
 
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @Setter
