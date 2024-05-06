c0c75fb1c9860b3dd392a869a693294e9650037e
shodiqbasiru <shodiqbasiru@gmail.com>
2024-05-06 16:44:27 +0700

    [IMPROVE] add some endpoint

--- src/main/java/com/enigma/laternak/constant/ApiRoute.java
+++ src/main/java/com/enigma/laternak/constant/ApiRoute.java
@@ -6,4 +6,7 @@ public class ApiRoute {
     public static final String STORE_API = "/api/stores";
     public static final String PRODUCT_API = "/api/products";
     public static final String CART_API = "/api/carts";
+    public static final String REVIEW_API = "/api/reviews";
+    public static final String IMAGE_PRODUCT_API = "/api/images/product";
+    public static final String IMAGE_USER_API = "/api/images/user";
 }
